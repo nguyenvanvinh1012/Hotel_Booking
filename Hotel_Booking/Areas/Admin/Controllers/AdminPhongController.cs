24e8c510b131c201fbe7b147938257efbe7de8d6
PhongSasuke <lethanhphong7080@gmail.com>
2023-04-18 06:44:15 +0700

    Phong

---
+++ Hotel_Booking/Areas/Admin/Controllers/AdminPhongController.cs
@@ -0,0 +1,67 @@
+﻿using Hotel_Booking.Models;
+using PagedList;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Hotel_Booking.Areas.Admin.Controllers
+{
+    public class AdminPhongController : Controller
+    {
+        HotelBookingContext context = new HotelBookingContext();
+
+        // GET: Admin/AdminPhong
+        public ActionResult Index(int? page)
+        {
+            int pageSize = 7;
+            int pageIndex = page.HasValue ? page.Value : 1;
+            var result = context.Phongs.ToList().ToPagedList(pageIndex, pageSize);
+            return View(result);
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            Phong thongTin = context.Phongs.FirstOrDefault(p => p.Id == id);
+            if (thongTin == null)
+            {
+                return HttpNotFound("Thông tin không tồn tại...!");
+            }
+            return View(thongTin);
+        }
+        [HttpPost]
+        public ActionResult Edit(Phong phong)
+        {
+            Phong editPhong = context.Phongs.FirstOrDefault(p => p.Id == phong.Id);
+            if (editPhong == null)
+            {
+                return HttpNotFound("Thông tin không tồn tại...!");
+            }
+            editPhong.Ten = phong.Ten;
+            editPhong.DienTich = phong.DienTich;
+            editPhong.GiaThue = phong.GiaThue;
+            editPhong.TienNghi = phong.TienNghi;
+            editPhong.MoTa = phong.MoTa;
+            editPhong.LoaiGiuong = phong.LoaiGiuong;
+            context.SaveChanges();
+            TempData["Message"] = "Chỉnh sửa thành công !";
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            ViewBag.ListKhachsan = context.KhachSans.ToList();
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Phong phong)
+        {
+            context.Phongs.Add(phong);
+            context.SaveChanges();
+            TempData["Message"] = "Tạo mới thành công !";
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Hotel_Booking/Areas/Admin/Controllers/SearchController.cs
@@ -0,0 +1,190 @@
+﻿using Hotel_Booking.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Razor.Tokenizer.Symbols;
+
+namespace Hotel_Booking.Areas.Admin.Controllers
+{
+    public class SearchController : Controller
+    {
+        // GET: Admin/Search
+        public HotelBookingContext context = new HotelBookingContext();
+        [HttpPost]
+        public ActionResult FindThanhPho(string keyword)
+        {
+            List<ThanhPho> list = new List<ThanhPho>();
+            if (string.IsNullOrEmpty(keyword) || keyword.Length < 1)
+            {
+                int count = 0;
+                List<ThanhPho> list2 = new List<ThanhPho>();
+                foreach (var item in context.ThanhPhoes.ToList())
+                {
+                    list2.Add(item);
+                    count++;
+                    if (count == 7)
+                        break;
+                }
+                return PartialView("FindThanhPho", list2);
+
+            }
+            list = context.ThanhPhoes.AsNoTracking()
+                //.Include(a => a.Id)
+                .Where(x =>x.Ten.Contains(keyword))
+                .OrderByDescending(x => x.Ten)
+                .Take(10)
+                .ToList();
+
+            if (list == null)
+            {
+                return PartialView("FindThanhPho", null);
+            }
+            else
+            {
+                return PartialView("FindThanhPho", list);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult FindKhachSan(string keyword)
+        {
+            List<KhachSan> list = new List<KhachSan>();
+            if (string.IsNullOrEmpty(keyword) || keyword.Length < 1)
+            {
+                int count = 0;
+                List<KhachSan> list2 = new List<KhachSan>();
+                foreach (var item in context.KhachSans.ToList())
+                {
+                    list2.Add(item);
+                    count++;
+                    if (count == 7)
+                        break;
+                }
+                return PartialView("FindKhachSan", list2);
+
+            }
+            list = context.KhachSans.AsNoTracking()
+                //.Include(a => a.LoaiKhachSan)
+                .Where(x => x.Ten.Contains(keyword))
+                .OrderByDescending(x => x.Ten)
+                .Take(10)
+                .ToList();
+
+            if (list == null)
+            {
+                return PartialView("FindKhachSan", null);
+            }
+            else
+            {
+                return PartialView("FindKhachSan", list);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult FindPhong(string keyword)
+        {
+            List<Phong> list = new List<Phong>();
+            if (string.IsNullOrEmpty(keyword) || keyword.Length < 1)
+            {
+                int count = 0;
+                List<Phong> list2 = new List<Phong>();
+                foreach (var item in context.Phongs.ToList())
+                {
+                    list2.Add(item);
+                    count++;
+                    if (count == 7)
+                        break;
+                }
+                return PartialView("FindPhong", list2);
+
+            }
+            list = context.Phongs.AsNoTracking()
+                //.Include(a => a.Id)
+                .Where(x => x.Ten.Contains(keyword))
+                .OrderByDescending(x => x.Ten)
+                .Take(10)
+                .ToList();
+
+            if (list == null)
+            {
+                return PartialView("FindPhong", null);
+            }
+            else
+            {
+                return PartialView("FindPhong", list);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult FindLoaiKhachSan(string keyword)
+        {
+            List<LoaiKhachSan> list = new List<LoaiKhachSan>();
+            if (string.IsNullOrEmpty(keyword) || keyword.Length < 1)
+            {
+                int count = 0;
+                List<LoaiKhachSan> list2 = new List<LoaiKhachSan>();
+                foreach (var item in context.LoaiKhachSans.ToList())
+                {
+                    list2.Add(item);
+                    count++;
+                    if (count == 7)
+                        break;
+                }
+                return PartialView("FindLoaiKhachSan", list2);
+
+            }
+            list = context.LoaiKhachSans.AsNoTracking()
+                //.Include(a => a.Id)
+                .Where(x => x.Ten.Contains(keyword))
+                .OrderByDescending(x => x.Ten)
+                .Take(10)
+                .ToList();
+
+            if (list == null)
+            {
+                return PartialView("FindLoaiKhachSan", null);
+            }
+            else
+            {
+                return PartialView("FindLoaiKhachSan", list);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult FindDatPhong(string keyword)
+        {
+            List<DatPhong> list = new List<DatPhong>();
+            if (string.IsNullOrEmpty(keyword) || keyword.Length < 1)
+            {
+                int count = 0;
+                List<DatPhong> list2 = new List<DatPhong>();
+                foreach (var item in context.DatPhongs.ToList())
+                {
+                    list2.Add(item);
+                    count++;
+                    if (count == 7)
+                        break;
+                }
+                return PartialView("FindLoaiKhachSan", list2);
+
+            }
+            list = context.DatPhongs.AsNoTracking()
+                //.Include(a => a.Id)
+                .Where(x => x.TaiKhoan.Contains(keyword))
+                .OrderByDescending(x => x.TaiKhoan)
+                .Take(10)
+                .ToList();
+
+            if (list == null)
+            {
+                return PartialView("FindLoaiKhachSan", null);
+            }
+            else
+            {
+                return PartialView("FindLoaiKhachSan", list);
+            }
+        }
+    }
+}
