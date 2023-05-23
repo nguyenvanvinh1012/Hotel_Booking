d0631e99260a378114748e5e94a4a26284f7663c
PhongSasuke <lethanhphong7080@gmail.com>
2023-05-23 20:52:31 +0700

    MaHoaThongTin

--- Hotel_Booking/Controllers/AccountController.cs
+++ Hotel_Booking/Controllers/AccountController.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
@@ -13,6 +15,114 @@ namespace Hotel_Booking.Controllers
         // GET: Account
 
         HotelBookingContext context = new HotelBookingContext();
+        public string MH(string mh)
+        {
+            byte[] temp = ASCIIEncoding.ASCII.GetBytes(mh);
+            byte[] hasData = new MD5CryptoServiceProvider().ComputeHash(temp);
+            string hasPass = "";
+            foreach (byte item in hasData)
+            {
+                hasPass += item;
+            }
+            return (hasPass);
+        }
+        public ActionResult Index()
+        {
+            var TK = Session["TenTaiKhoan"];
+            if (TK != null)
+            {
+                var taikhoan = context.TaiKhoans.FirstOrDefault(p => p.TenTaiKhoan == TK);
+                if (taikhoan != null)
+                {
+                    var lsDatPhong = context.DatPhongs.Where(p => p.TaiKhoan == taikhoan.TenTaiKhoan).ToList();
+                    ViewBag.DatPhong = lsDatPhong;
+                    return View(taikhoan);
+                }
+
+            }
+            return RedirectToAction("DangNhap");
+        }
+
+        [HttpGet]
+        public ActionResult Edit()
+        {
+            var TK = Session["TenTaiKhoan"];
+            if (TK != null)
+            {
+                var taikhoan = context.TaiKhoans.FirstOrDefault(p => p.TenTaiKhoan == TK);
+                if (taikhoan != null)
+                {
+                    var lsDatPhong = context.DatPhongs.Where(p => p.TaiKhoan == taikhoan.TenTaiKhoan).ToList();
+                    ViewBag.DatPhong = lsDatPhong;
+                    return View(taikhoan);
+                }
+
+            }
+            return RedirectToAction("DangNhap");
+        }
+
+        [HttpPost]
+        public ActionResult Edit(TaiKhoan taiKhoan, string MKhienTai, string xacNhanMKmoi, string MKmoi)
+        {
+            TaiKhoan tk = context.TaiKhoans.FirstOrDefault(p => p.TenTaiKhoan == taiKhoan.TenTaiKhoan);
+            TaiKhoan tk1 = new TaiKhoan();
+            if (tk != null)
+            {
+                if (MKhienTai == "")
+                {
+                    tk.HoTen = taiKhoan.HoTen;
+                    tk.SoDienThoai = taiKhoan.SoDienThoai;
+                    tk.Email = taiKhoan.Email;
+                    context.SaveChanges();
+                    TempData["Message"] = "Chỉnh sửa thành công !";
+                    return RedirectToAction("Edit", "Account");
+                }
+                else
+                {
+                    if (MH(MKhienTai) == tk.MatKhau && MKmoi != "")
+                    {
+                        if (MH(MKmoi) != tk.MatKhau && MH(xacNhanMKmoi) == MH(MKmoi))
+                        {
+                            tk.HoTen = taiKhoan.HoTen;
+                            tk.SoDienThoai = taiKhoan.SoDienThoai;
+                            tk.Email = taiKhoan.Email;
+                            tk.MatKhau = MH(MKmoi);
+                            context.SaveChanges();
+                            TempData["Message"] = "Chỉnh sửa thành công !";
+                            return RedirectToAction("Edit", "Account");
+                        }
+                        else
+                        {
+                            TempData["MessageErr"] = "Xác nhận sai mật khẩu mới !";
+                            return RedirectToAction("Edit", "Account");
+                        }
+                    }
+                    else
+                    {
+                        TempData["MessageErr"] = "Mật khẩu chưa đúng !";
+                        return RedirectToAction("Edit", "Account");
+                    }
+                }
+
+            }
+            return HttpNotFound();
+        }
+        public ActionResult Oder()
+        {
+            var TK = Session["TenTaiKhoan"];
+            if (TK != null)
+            {
+                var taikhoan = context.TaiKhoans.FirstOrDefault(p => p.TenTaiKhoan == TK);
+                if (taikhoan != null)
+                {
+                    var lsDatPhong = context.DatPhongs.Where(p => p.TaiKhoan == taikhoan.TenTaiKhoan).ToList();
+                    ViewBag.DatPhong = lsDatPhong;
+                    return View(taikhoan);
+                }
+
+            }
+            return RedirectToAction("DangNhap");
+        }
         [HttpGet]
         public ActionResult DangKy()
         {
